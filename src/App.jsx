7cd66d3f9a8faea02652781c59241e3e0bfe2d43
Furqan123456777 <158835274+Furqan123456777@users.noreply.github.com>
2024-06-29 16:45:18 +0500

    ok

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1><i>My First React Project </i></h1>
+      <hr />
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, aliquam.</p>
+      <hr />
+      <div>
+      <img width={150} height={200} src="/vite.svg" alt="" />
+      <img width={270} height={200} src="https://cdn-blog.lawrencemcdaniel.com/wp-content/uploads/2020/06/09140550/React-logo.png" alt="" />
+      </div>
+
+    </div>
+  )
+    }
+
+export default App
